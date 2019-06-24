f2cc59cb94200c29362f533f5e3b4015d5f12e0f
“CGWTQe <375091805@qq.com>
2019-06-24 23:43:45 +0800

    2016-06-24 首页页面制作

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,42 @@
 import React from 'react'
+import { BrowserRouter as Router, Route, Link, HashRouter } from "react-router-dom"
 
-export default class App extends React.Component{
-  constructor(props){
+//antd布局
+import { Layout, Menu } from 'antd';
+const { Header, Content, Footer } = Layout;
+
+//导入scss
+import style from './css/app.scss'
+
+
+export default class App extends React.Component {
+  constructor(props) {
     super(props)
 
     this.state = {}
   }
-  
-  render(){
-    return <div>
-      App
-    </div>
-  }
 
-  
+  render() {
+    return <HashRouter>
+        <Layout className="layout" style={{height:'730px'}}>
+        <Header>
+          <div className={style.logo} />
+          <Menu
+            theme="dark"
+            mode="horizontal"
+            defaultSelectedKeys={['1']}
+            style={{ lineHeight: '64px' }}
+          >
+            <Menu.Item key="1">首页</Menu.Item>
+            <Menu.Item key="2">电影</Menu.Item>
+            <Menu.Item key="3">关于</Menu.Item>
+          </Menu>
+        </Header>
+        <Content style={{ background: '#fff' }}>
+          1233
+        </Content>
+        <Footer style={{ textAlign: 'center' }}>CGWTQ ©2019 React Shop</Footer>
+      </Layout>     
+    </HashRouter>
+  }
 }
